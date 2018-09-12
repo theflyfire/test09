6f43a1e944ef8d11f26ccda1b61c1f0a5fc8baa7
Daming <Daming@233.com>
2018-09-12 21:12:20 +0800

    adasd

--- src/main/java/com/zdx/firs/MainDev.java
+++ src/main/java/com/zdx/firs/MainDev.java
@@ -2,6 +2,6 @@ package com.zdx.firs;
 
 public class MainDev {
 	public static void main(String[] args) {
-		System.out.println("MainDev....sdf...sddf.................");
+		System.out.println("MainDev....sdf...sddf....hot_fix.............");
 	}
 }
